27013aab789b1049e63a32cf6f44b81493af05c0
Ruan Pablo <ruan_goio10@hotmail.com>
2022-02-26 22:27:37 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../src/styles/global.scss'
+import { Counter } from './components/Counter';
 
 import { RepositoryList } from './components/RepositoryList';
 
 export function App() {
     return (
-        <RepositoryList />
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+
     )
 }
